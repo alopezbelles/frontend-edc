4199cf1c0e85fa497905f35847f2434be702c7b2
Alex <alopezbelles@gmail.com>
2023-08-03 17:56:21 +0200

    deploying to AWS

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+
+module.exports = {
+  publicPath: '/', // Establece la ruta base de tu aplicación (debe ser '/' en la mayoría de los casos)
+  transpileDependencies: true,
+};
